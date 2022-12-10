7a83e3c888022857dc5bfc9c34d9a3a13cc93f57
Arif Uddin <mr.uddin360@gmail.com>
2022-12-10 18:38:29 +0600

    home and navbar components added

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        myportfoliotheme:{
+          primary: '#60a5fa',
+          secondary: '#19D3AE',
+          accent: "#64748b",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
+  plugins: [require("daisyui")],
+}
